7d3c9983fa167ac45d9eaa5ef4e5f7edafc9df2b
BreaQ <91721929+BreaQ@users.noreply.github.com>
2021-11-30 20:27:27 +0100

    Weather API change
    And some fixes to weather. It should now check the long and lat location and than fetch data from new api.
    
    Skycons deleted, now it downloads img src of condition stright from api.

--- HealthApp/weather.js
+++ HealthApp/weather.js
@@ -5,6 +5,7 @@ window.addEventListener("load", () => {
   let temperatureDegree = document.querySelector('.temperature-degree');
   let locationTimezone = document.querySelector('.location-timezone');
   let temperatureSection = document.querySelector('.temperature');
+  let conditionIcon = document.querySelector('.icon')
   const temperatureSpan = document.querySelector('.temperature span');
   // round to 2 decimal places (for fahrenheit)
   function roundToTwo(num) {    
@@ -17,19 +18,19 @@ window.addEventListener("load", () => {
       lat = position.coords.latitude;
       console.log(long,lat);
       const api =
-        'https://api.weatherapi.com/v1/current.json?key=b8a998bfdaa54513985190258212610&q=52.25,21&aqi=yes';
+        `https://api.weatherapi.com/v1/current.json?key=b8a998bfdaa54513985190258212610&q=${roundToTwo(lat)}.${roundToTwo(long)}&aqi=yes`;
       fetch(api)
         .then(response => {
           return response.json();
         })
         .then(data => {
-          const {temp_c, conditionText, icon} = data;
-          console.log(data);
-          temperatureDegree.textContent = temp_c;
-          temperatureDescription.textContent = condition.Text;
-          locationTimezone.textContent = tz_id.replace(/_/," ");
-          let fahrenheit = (temp_c -32) *  (5/9);
-          setIcons(icon, document.querySelector(".icon"));
+          const {current, location} = data;
+          console.log(api);
+          temperatureDegree.textContent = current.temp_c;
+          temperatureDescription.textContent = current.condition.text;
+          locationTimezone.textContent = location.tz_id.replace("/"," - ");
+          conditionIcon.src = "https:"+current.condition.icon;
+          let fahrenheit = (current.temp_c -32) *  (5/9);
           temperatureSection.addEventListener('click', () =>{
             if(temperatureSpan.textContent === "°C"){
               temperatureSpan.textContent = "°F";
@@ -40,19 +41,12 @@ window.addEventListener("load", () => {
               temperatureDegree.textContent = temp_c;
             }
           })
-          if (temp_c >= 10 && conditions === "Clear"){
+          if (current.temp_c >= 10 && current.condition.text === "Clear"){
             alert("This is a great day for your running training!")
           }
 
         });
     });
   } 
-  function setIcons (icon, iconID){
-    const skycons = new Skycons({color:"white"});
-    const currentIcon = icon.replace(/-/,"_").toUpperCase();
-    skycons.play();
-    return skycons.set(iconID, Skycons[currentIcon]);
-
-  }
 });
 
